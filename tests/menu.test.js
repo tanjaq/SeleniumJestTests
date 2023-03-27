8ea9e18bbe86493ca2d8d2f7627307f334f75398
Gopnik987 <danikusov@yandex.ru>
2023-03-27 14:21:44 +0300

    Task done

--- tests/menu.test.js
+++ tests/menu.test.js
@@ -11,7 +11,7 @@ describe("Search products from menu", () => {
     driver = await new Builder()
       .forBrowser("chrome")
       // If you dont want to open browser, uncomment following row
-      .setChromeOptions(new chrome.Options())
+      //.setChromeOptions(new chrome.Options().addArguments('--headless'))
       .build();
     driver
       .manage()
@@ -29,11 +29,10 @@ describe("Search products from menu", () => {
   });
 
   afterAll(async () => {
-    //await driver.quit()
+    await driver.quit()
   });
 
   test("Test Open Web Page", async () => {
-    //Verify that the web page has a Book Depository title.
     const pageTitle = await driver
       .findElement(By.xpath('//h1/a[@class="brand-link"]/img'))
       .getAttribute("alt");
@@ -90,7 +89,6 @@ describe("Search products from menu", () => {
     searchCountNum = parseInt(searchCount.replace(",", ""));
 
     expect(searchCountNum).toBeGreaterThan(1);
-    //Verify that there are more than 1 products found.
   });
 
   test("Test Select Beverages", async () => {
@@ -120,5 +118,4 @@ describe("Search products from menu", () => {
     let links = await driver.findElements(By.className("parent-item"));
     expect(links.length).toEqual(4);
   });
-  //body > div.page-slide > div.content-wrap > div.main-content.category-page > div.block-wrap > h1
 });
