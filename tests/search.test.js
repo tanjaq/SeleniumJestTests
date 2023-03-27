908d015acd1af069e4a6dd595799661eeacfe3fc
tatjana <tatjana.kruglova1@gmail.com>
2023-03-27 14:35:04 +0300

    Refactored tests to use POM

--- tests/search.test.js
+++ tests/search.test.js
@@ -2,8 +2,9 @@ const {Builder, By} = require('selenium-webdriver')
 const chrome = require('selenium-webdriver/chrome')
 require('chromedriver')
 
+let HomePage = require('../pageobjects/homePage')
+
 const TIMEOUT = 50000
-let searchCountNum
 
 describe('Search products', () => {
 
@@ -18,8 +19,11 @@ describe('Search products', () => {
         driver.manage().setTimeouts({implicit: TIMEOUT, pageLoad: TIMEOUT, script: TIMEOUT})
         driver.manage().window().maximize()
 
-        await driver.get('https://www.bookdepository.com/')
-        await driver.findElement(By.css('div.cookie-consent > div.cookie-consent-buttons > button.btn.btn-sm.btn-yes')).click()
+        HomePage = new HomePage(driver)
+
+        await HomePage.openUrl()
+        await HomePage.agreeWithCookies()
+        
     })
     
     afterAll(async () => {
@@ -28,69 +32,30 @@ describe('Search products', () => {
 
 
     test('Test Open Web Page', async () => {
-        //Verify that the web page has a Book Depository title.
-        const pageTitle = await driver.findElement(By.xpath('//h1/a[@class="brand-link"]/img')).getAttribute('alt')
-        expect(pageTitle).toContain('Bookdepository.com')
+        await HomePage.verifyPageTitleContains('Bookdepository.com')
     })
 
     test('Test Search by Keyword', async () => {
-        const searchField = await driver.findElement(By.css('#book-search-form > div > input[name="searchTerm"]'))
-        searchField.click()
-        searchField.sendKeys('Harry Potter')
-        await driver.findElement(By.className('header-search-btn')).click()
 
-        const searchResultTitle = await driver.findElement(By.css('div.main-content.search-page > h1')).getText()
-        expect(searchResultTitle).toContain('Search results for Harry Potter')
+        await HomePage.searchForText('Harry Potter')
+        await HomePage.verifySearchResultText('Harry Potter')
+        await HomePage.verifyAllSearchItemsContainText('Harry Potter')
+        await HomePage.verifySearchResultContainsMoreItemsThan(1)
 
-        const searchCount = await driver.findElement(By.className('search-count')).getText()
-        searchCountNum = parseInt(searchCount.replace(',', ''))
-
-        //Verify that there are more than 1 products found.
-        expect(searchCountNum).toBeGreaterThan(1)
-
-        let itemFormats = await driver.findElements(By.css('div.item-info > h3.title'))
-
-        //Verify that products presented have searched keyword in it.
-        for(let item of itemFormats) {
-            expect(await item.getText()).toContain('Harry Potter')
-        }
     })
 
     test('Test Sort by price', async () => {
-        //Verify that found products can be sorted.
-        let sortByOptions = await driver.findElements(By.xpath('//select[@name="searchSortBy"]/option'))
-        expect(sortByOptions).toHaveLength(5)
 
-        await driver.findElement(By.xpath('//select[@name="searchSortBy"]/option[@value="price_high_low"]')).click()
+        await HomePage.verifyProductSortOptions();
+        await HomePage.sortResultsByPrice();
+        await HomePage.verifyResultsAreSorted();
 
-        let searchItems = await driver.findElements(By.css('span.sale-price'))
-        
-        //Verify that the products are sorted correctly.
-        let price1 = parseFloat((await searchItems[0].getText()).replace(/[^\d,.]/, '').replace(',', '.'))
-        let price2 = parseFloat((await searchItems[1].getText()).replace(/[^\d,.]/, '').replace(',', '.'))
-
-        expect(price1).toBeGreaterThanOrEqual(price2)
     })
 
     test('Test Filter by format', async () => {
-        //Verify that products can be filtered by 6 categories.
-        let filterByOptions = await driver.findElements(By.css('form.filter-menu > div > label'))
-        expect(filterByOptions).toHaveLength(6)
-
-        await driver.findElement(By.xpath('//*[@id="filterFormat"]/option[contains(text(),"Hardback")]')).click()
-        await driver.findElement(By.xpath('//button[contains(text(),"Refine results")]')).click()
-
-        //Verify that items selected have correct format.        
-        let itemFormats = await driver.findElements(By.css('div.item-info > p.format'))
-
-        for(let item of itemFormats) {
-            expect(await item.getText()).toContain('Hardback')
-        }
-
-        //Verify that products list contains less items now.
-        const searchCountFiltered = await driver.findElement(By.className('search-count')).getText()
-        const searchCountFilteredNum = parseInt(searchCountFiltered.replace(',', ''))
-
-        expect(searchCountFilteredNum).toBeLessThan(searchCountNum)
+        await HomePage.verifyProductFilters();
+        await HomePage.filterResultsByText('Hardback');
+        await HomePage.verifyResultsFilter('Hardback');
+        await HomePage.verifyResultsAreFiltered();
     })
 })
